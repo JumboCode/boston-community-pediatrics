36679b3f7761c80013b784259848d200590b0c62
PLlamas28 <Peter.Llamas@tufts.edu>
2025-10-18 21:30:14 -0400

    Ticket 1 - button component
    Eddy and I made a default button based on the figma design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+'use client';
+import Button from "@/components/common/buttons/Button";
+
+const Home: React.FC = () => {
+
+  const handleClick = () => {
+    console.log(`Button clicked!`);
+  };
+  
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-br from-blue-400 to-purple-600 text-white">
       {/* Top spacer */}
@@ -9,12 +17,14 @@ export default function Home() {
         <h1 className="text-5xl font-extrabold animate-bounce">
           Welcome to BCP!
         </h1>
+        <Button label={"Default"} onClick={handleClick}/>
         <p className="text-xl text-white/90">
           Your go-to platform for managing volunteers, events, and more.
         </p>
         <button className="bg-white text-blue-600 font-bold py-2 px-6 rounded-lg shadow-lg hover:bg-gray-200 transition-all">
           Get Started
         </button>
+        
       </div>
 
       {/* Bottom spacer */}
@@ -22,3 +32,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
--- src/components/common/buttons/Button.tsx
+++ src/components/common/buttons/Button.tsx
@@ -1,11 +1,27 @@
 // Button component! 
 import React from "react";
+import Image from 'next/image';
 
 interface Props {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
+  leftIconPath?: string;
+  rightIconPath?: string;
+  altStyle?: string;
+  altTextStyle?: string;
+  disabled?: boolean;
+  type?: HTMLButtonElement["type"];
 }
 
-export default function Button(props: Props) {
-  return <button onClick={props.onClick}>{props.label}</button>;
-}
+const Button: React.FC<Props> = (props) => {
+  return (
+    <button onClick={props.onClick} disabled={props.disabled} className={props.altStyle ?  props.altStyle : "text-white-600 bg-[#234254] font-medium rounded text-sm px-5.5 py-3 text-center hover:bg-[#4B7B96]-600 focus:outline-[#1a91d6] focus:bg-[#234254] active:bg-[#071823] disabled:opacity-75 disabled:bg-[#6D808B]"}>
+    
+      {props.leftIconPath ? <Image src={props.leftIconPath} alt={props.altTextStyle ? props.altTextStyle:""}/>:null}
+      {props.label}
+      {props.rightIconPath ? <Image src={props.rightIconPath} alt=""/>:null}
+    </button>
+  );
+};
+
+export default Button;
