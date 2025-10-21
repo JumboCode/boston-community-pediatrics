59bf77a2ae13a44294d078232985cfdb551bc50b
jeremiahlongino <jeremiah.longino04@gmail.com>
2025-10-21 00:11:36 -0400

    Done with Navbar and Footer Ticket
    Footer should always be at bottom, NavBar looks close to Figma, and we put in some things to help with different screen sizes.
    
    Made some changes to page.tsx so that we can show the navbar and footer and get rid of placeholder content.
    
    Added just some pictures from the figma for the profile placeholder pictures

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+      <footer className="bg-[#234254] bottom-0 w-full flex items-center justify-between p-6"> 
+      <p className="text-xs text-white">© 2025 Boston Community Pediatrics</p> 
+    </footer> //Changed to className to keep language consistent and removed 'fixed' - Jeremiah
+  );
+};
+
+export default Footer;
+
