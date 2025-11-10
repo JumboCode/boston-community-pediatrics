f3b2d7b91eb146b4ac37b4477fccc504ff60c78e
jeremiahlongino <jeremiah.longino04@gmail.com>
2025-11-09 23:49:40 -0500

    Finalized Changes for EventAdminTable
    Addressed comments by adding Select All button functionality, fixing a case in the status bar for 0 total slots, and making code more uniform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,18 @@
-"use client";
-import React, { useState } from "react";
+'use client';
+import React from "react";
 import Button from "@/components/common/buttons/Button";
 import EventAdminTable from "@/components/common/tables/EventAdminTable";
 
-interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
-  emailAddress: string;
-  phoneNumber: string;
-  selected: boolean;
-}
-
 const Home: React.FC = () => {
-  const [volunteers, setVolunteers] = useState<User[]>([
-    {
-      id: "1",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-    {
-      id: "2",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-    {
-      id: "3",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-    {
-      id: "4",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-    {
-      id: "5",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-    {
-      id: "6",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-    {
-      id: "7",
-      firstName: "Participant",
-      lastName: "Name",
-      emailAddress: "participant@email.com",
-      phoneNumber: "123-456-7890",
-      selected: false,
-    },
-  ]);
-
-  const toggleSelect = (id: string) => {
-    setVolunteers((prev) =>
-      prev.map((v) => (v.id === id ? { ...v, selected: !v.selected } : v))
-    );
-  };
-
   const handleClick = () => {
-    console.log(`Button clicked!`);
+    console.log("Button clicked!");
   };
 
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center p-8">
-      <div className="w-[996px] bg-white border border-black shadow-md font-sans z-10">
-        {/* Header Section */}
-        <div className="flex flex-col md:flex-row justify-between mb-6">
-          {/* Left */}
-          <div className="space-y-2 text-gray-600 px-5 pt-5">
-            <h1 className="text-[24px] font-semibold">Position Name</h1>
-            <p>123 ABC Street, Boston MA 12345</p>
-            <p>10:00AM - 3:00PM</p>
-            <p className="text-[24px] font-medium">12/23 Spots Filled</p>
-          </div>
-
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-br from-blue-400 to-purple-600 text-white">
+      {/* Top spacer */}
+      <div></div>
+      
       {/* Main content */}
       {/* <div className='text-center space-y-6'>
         <h1 className='text-5xl font-extrabold animate-bounce'>
@@ -106,7 +24,7 @@ const Home: React.FC = () => {
         <button className='bg-white text-blue-600 font-bold py-2 px-6 rounded-lg shadow-lg hover:bg-gray-200 transition-all'>
           Get Started
         </button> */}
-      <div className="min-h-screen bg-white flex items-center justify-center p-6">
+      <div className="min-h-screen flex items-center justify-center p-6">
         <EventAdminTable
           position="Position Name"
           startTime="10:00 AM"
@@ -119,16 +37,14 @@ const Home: React.FC = () => {
           filledSlots={12}
           totalSlots={23}
           location="123 ABC Street, Boston MA 12345"
-          volunteers={volunteers}
-          toggleSelect={toggleSelect}
         ></EventAdminTable>
+        
         {/* Bottom spacer */}
         <div></div>
+        
+        </div>
       </div>
-    </div>
-    </div>
-    </div>
   );
-};
+}
 
 export default Home;
