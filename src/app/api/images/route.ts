115146b1d7aa9fda3f2d7eeaac88b140b1c495be
jeremiahlongino <jeremiah.longino04@gmail.com>
2025-11-14 16:09:08 -0500

    Initial Setup of Image R2 Endpoints
    Created controller.ts, route.ts, and r2.ts and started using aws

---
+++ src/app/api/images/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+
+
+export async function GET(req: NextRequest) {
+}
+
+export async function POST(req: NextRequest) {
+}
+
+export async function DELETE(req: NextRequest) {
+}
