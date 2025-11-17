19a8edde98b97af2b16d77c3eef87b51e666e3d8
logany <logan.yuan@gmail.com>
2025-11-16 19:26:29 -0500

    update: shii idek

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,5 +3,5 @@
 import { SignUp } from "@stackframe/stack";
 
 export default function LoginPage() {
-  return <SignUp fullPage automaticRedirect={true}/>;
+  return <SignUp fullPage automaticRedirect={true} />;
 }
