78e12b8fcf6efd3c23f0fc9fd1f50737f7e8a4fc
tomasmaranga <tomas.maranga@icloud.com>
2025-11-21 13:19:02 -0500

    finsihed up routes

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -1,19 +1,53 @@
 import {
-    S3Client,
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 
-const ACCOUNT_ID = process.env.ACCOUNT_ID as string;
-const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID as string;
-const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY as string;
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
+const ACCESS_KEY_ID = process.env.R2_ACCESS_KEY!;
+const SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY!;
+const ENDPOINT = process.env.R2_ENDPOINT!;
+const BUCKET = process.env.R2_BUCKET!;
 
-const S3 = new S3Client({
-    region: "auto",
-    endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
-    credentials: {
-        accessKeyId: ACCESS_KEY_ID,
-        secretAccessKey: SECRET_ACCESS_KEY,
-    },
+export const S3 = new S3Client({
+  region: "auto",
+  endpoint: ENDPOINT,
+  credentials: {
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
 });
+
+export async function getPresignedURL(filename: string) {
+  const url = getSignedUrl(
+    S3,
+    new PutObjectCommand({
+      Bucket: BUCKET,
+      Key: filename,
+    }),
+    {
+      expiresIn: 600,
+    }
+  );
+  return url;
+}
+
+export function getPublicURL(filename: string) {
+  const endpointURL = new URL(ENDPOINT);
+  const host = endpointURL.host;
+
+  return `https://${BUCKET}.${host}/${filename}`;
+}
+
+export async function deleteObject(filename: string) {
+  await S3.send(
+    new DeleteObjectCommand({
+      Bucket: BUCKET,
+      Key: filename,
+    })
+  );
+}
 
 export default S3;
