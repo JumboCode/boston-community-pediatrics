10a8e613992d6ef2794f99af73d9fb041151dd64
ManuP6789 <manuel.pena654576@tufts.edu>
2025-12-06 18:34:12 -0500

    duplicate stuff

--- src/app/api/eventSignup/controller.ts
+++ src/app/api/eventSignup/controller.ts
@@ -40,29 +40,6 @@ function publicUser(user: User): PublicUser {
   };
 }
 
-// Fetch user signups by event position
-export async function getUsersByPositionId(positionId: string) {
-  const signups = await prisma.eventSignup.findMany({
-    where: { positionId },
-    include: { user: true },
-  });
-  
-
-   const users = signups
-    .map((s) => s.user)
-    .filter((u): u is User => u !== null) 
-    .map(publicUser);
-  return users;
-}
-
-function publicUser(user: User) {
-  return {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-  };
-}
-
 // Create event signup
 export const createEventSignup = async (
   data: Prisma.EventSignupCreateInput
