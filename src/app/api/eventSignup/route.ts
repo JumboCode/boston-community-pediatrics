e7144a677e65308552a515e7c56605749d4b8282
ManuP6789 <manuel.pena654576@tufts.edu>
2025-12-06 18:37:59 -0500

    eslint stuff 👎

--- src/app/api/eventSignup/route.ts
+++ src/app/api/eventSignup/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import {
   getSignupsByEventId,
-  getSignupsByPositionId,
   getUsersByPositionId,
   createEventSignup,
   updateEventSignup,
