16864d1b22f444bc6ce777add929555c1b4a831e
jeremiahlongino <jeremiah.longino04@gmail.com>
2026-01-07 00:52:01 -0500

    BCP 28 Image R2 Endpoints Setup  (#31)
    * Initial Setup of Image R2 Endpoints
    
    Created controller.ts, route.ts, and r2.ts and started using aws
    
    * finsihed up routes
    
    * removed controller.ts
    
    * Updated r2.ts
    
    Just added await for syntax
    
    * trying a fix
    
    * really fixing package lock
    
    * fix versions for merge
    
    * build issue
    
    ---------
    
    Co-authored-by: tomasmaranga <tomas.maranga@icloud.com>
    Co-authored-by: ManuP6789 <manuel.pena654576@tufts.edu>

---
+++ src/lib/r2.ts
@@ -0,0 +1,53 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const ACCESS_KEY_ID = process.env.R2_ACCESS_KEY!;
+const SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY!;
+const ENDPOINT = process.env.R2_ENDPOINT!;
+const BUCKET = process.env.R2_BUCKET!;
+
+export const S3 = new S3Client({
+  region: "auto",
+  endpoint: ENDPOINT,
+  credentials: {
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+});
+
+export async function getPresignedURL(filename: string) {
+  const url = await getSignedUrl(
+    S3,
+    new PutObjectCommand({
+      Bucket: BUCKET,
+      Key: filename,
+    }),
+    {
+      expiresIn: 600,
+    }
+  );
+  return url;
+}
+
+export function getPublicURL(filename: string) {
+  const endpointURL = new URL(ENDPOINT);
+  const host = endpointURL.host;
+
+  return `https://${BUCKET}.${host}/${filename}`;
+}
+
+export async function deleteObject(filename: string) {
+  await S3.send(
+    new DeleteObjectCommand({
+      Bucket: BUCKET,
+      Key: filename,
+    })
+  );
+}
+
+export default S3;
