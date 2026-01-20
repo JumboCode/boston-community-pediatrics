f266484658e5d49a2a230cebcc6810f912b272e3
nataliearai <hanakonat@gmail.com>
2026-01-19 21:07:47 -0500

    tried

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,16 @@
+"use client";
 import Image from "next/image";
 import BackArrow from "@/assets/icons/arrow-left.svg";
 import EventCard from "@/components/events/EventCard";
+import { useUser } from "@clerk/nextjs";
 
 export default function ProfilePage() {
-    
-    <EventCard
-        id="test-event"
-        title="Test Event"
-        image="/event1.jpg"   // must exist in /public
-        time={new Date()}
-        location="Test Location"
-        date={new Date()}
-    />
+    const { user, isSignedIn } = useUser();
+    const firstName = user?.firstName ?? "Guest";
+    const lastName = user?.lastName ?? "Guest";
+    const emailAddress = user?.primaryEmailAddress ?? "Guest";
+    const phoneNumber = user?.primaryPhoneNumber ?? "Guest";
+    //const accountCreated = 
 
     return (
         <main className="min-h-screen p-8">
@@ -30,24 +29,12 @@ export default function ProfilePage() {
                     <div className="h-[168px] w-[282px] bg-[#D9D9D9]" />
                     <div className="mt-[12px] flex items-center justify-between">
                         <div className="text-[20px] font-bold">Event 1</div>
-                        <button
-                            type="button"
-                            className="rounded p-1 hover:bg-gray-100"
-                        >
-                            <span className="text-3xl leading-none">⋮</span>
-                        </button>
                     </div>
                 </div>
                 <div className="w-[282px]">
                     <div className="h-[168px] w-[282px] bg-[#D9D9D9]" />
                     <div className="mt-[12px] flex items-center justify-between">
                         <div className="text-[20px] font-bold">Event 2</div>
-                        <button
-                            type="button"
-                            className="rounded p-1 hover:bg-gray-100"
-                        >
-                            <span className="text-3xl leading-none">⋮</span>
-                        </button>
                     </div>
                 </div>
             </div>
@@ -56,18 +43,18 @@ export default function ProfilePage() {
                 <div className="absolute top-[30px] left-1/2 transform -translate-x-1/2 w-[105px] h-[105px] bg-[#D9D9D9] rounded-full">
                 </div>
                 <div className = "flex flex-col items-center mt-40 space-y-.25">
-                    <div className = "text-white font-bold text-[24px]">NAME</div>
+                    <div className = "text-white font-bold text-[24px]">{firstName} {lastName}</div>
                     <div className = "text-white text-[16px]">Member since 0000</div>
                     
                 </div>
                 <div className = "flex flex-col mt-6 space-y-2">
                     <div className = "flex justify-between">
                         <div className = "text-white ml-[25px] text-[16px]">Phone number</div>
-                        <div className = "text-white mr-[25px] text-[16px]">123 456 7890</div>
+                        <div className = "text-white mr-[25px] text-[16px]">{phoneNumber}</div>
                     </div>
                     <div className = "flex justify-between">
                         <div className = "text-white ml-[25px] text-[16px]">Email</div>
-                        <div className = "text-white mr-[25px] text-[16px]">...@gmail.com</div>
+                        <div className = "text-white mr-[25px] text-[16px]">{emailAddress}</div>
                     </div>
                 </div>
                 
