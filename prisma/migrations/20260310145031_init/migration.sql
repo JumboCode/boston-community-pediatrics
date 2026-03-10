75ff3ad48b83f4d295d59fbee3abe65ab3ee371a
ManuP6789 <manuel.pena654576@tufts.edu>
2026-03-10 14:56:06 -0400

    fixed prisma

--- prisma/migrations/20260310145031_init/migration.sql
+++ prisma/migrations/20260310145031_init/migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE "users" (
     "clerkId" TEXT NOT NULL DEFAULT 'placeholder',
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "emailAddress" TEXT,
+    "emailAddress" TEXT NOT NULL,
     "phoneNumber" TEXT NOT NULL,
     "dateOfBirth" TIMESTAMP(3),
     "streetAddress" TEXT,
@@ -19,6 +19,8 @@ CREATE TABLE "users" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "role" "UserRole" NOT NULL,
+    "profileImage" TEXT,
+    "speaksSpanish" BOOLEAN,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -31,14 +33,17 @@ CREATE TABLE "Event" (
     "date" TIMESTAMP(3)[],
     "startTime" TIMESTAMP(3) NOT NULL,
     "endTime" TIMESTAMP(3) NOT NULL,
-    "address" TEXT NOT NULL,
-    "street" TEXT,
-    "city" TEXT,
-    "state" TEXT,
-    "country" TEXT,
-    "zipCode" TEXT,
+    "city" TEXT NOT NULL DEFAULT '',
+    "state" TEXT NOT NULL DEFAULT '',
+    "country" TEXT NOT NULL DEFAULT '',
+    "zipCode" TEXT NOT NULL DEFAULT '',
     "lat" DOUBLE PRECISION,
     "lng" DOUBLE PRECISION,
+    "addressLine1" TEXT NOT NULL DEFAULT '',
+    "addressLine2" TEXT,
+    "images" TEXT[],
+    "pinned" BOOLEAN NOT NULL DEFAULT false,
+    "imagesDeleted" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
 );
@@ -53,6 +58,7 @@ CREATE TABLE "EventSignup" (
     "date" TIMESTAMP(3),
     "time" TIMESTAMP(3),
     "notes" TEXT,
+    "comments" TEXT,
 
     CONSTRAINT "EventSignup_pkey" PRIMARY KEY ("id")
 );
@@ -68,8 +74,12 @@ CREATE TABLE "EventPosition" (
     "description" TEXT NOT NULL,
     "filledSlots" INTEGER NOT NULL,
     "totalSlots" INTEGER NOT NULL,
-    "lat" DOUBLE PRECISION,
-    "lng" DOUBLE PRECISION,
+    "addressLine1" TEXT NOT NULL DEFAULT '',
+    "addressLine2" TEXT,
+    "city" TEXT NOT NULL DEFAULT '',
+    "country" TEXT NOT NULL DEFAULT '',
+    "state" TEXT NOT NULL DEFAULT '',
+    "zipCode" TEXT NOT NULL DEFAULT '',
 
     CONSTRAINT "EventPosition_pkey" PRIMARY KEY ("id")
 );
@@ -84,6 +94,8 @@ CREATE TABLE "Guest" (
     "relation" TEXT,
     "phoneNumber" TEXT,
     "signupId" TEXT NOT NULL,
+    "dateOfBirth" TEXT,
+    "speaksSpanish" BOOLEAN,
 
     CONSTRAINT "Guest_pkey" PRIMARY KEY ("id")
 );
@@ -93,7 +105,9 @@ CREATE TABLE "EventWaitlist" (
     "id" TEXT NOT NULL,
     "userId" TEXT,
     "positionId" TEXT NOT NULL,
+    "comments" TEXT,
     "isGuest" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(6),
 
     CONSTRAINT "EventWaitlist_pkey" PRIMARY KEY ("id")
 );
@@ -106,6 +120,8 @@ CREATE TABLE "WaitlistGuest" (
     "email" TEXT,
     "relation" TEXT,
     "waitlistId" TEXT NOT NULL,
+    "dateOfBirth" TEXT,
+    "speaksSpanish" BOOLEAN,
 
     CONSTRAINT "WaitlistGuest_pkey" PRIMARY KEY ("id")
 );
@@ -113,26 +129,30 @@ CREATE TABLE "WaitlistGuest" (
 -- CreateIndex
 CREATE UNIQUE INDEX "users_emailAddress_key" ON "users"("emailAddress");
 
--- AddForeignKey
-ALTER TABLE "EventSignup" ADD CONSTRAINT "EventSignup_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- CreateIndex
+CREATE INDEX "users_clerkId" ON "users"("clerkId");
 
 -- AddForeignKey
-ALTER TABLE "EventSignup" ADD CONSTRAINT "EventSignup_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EventSignup" ADD CONSTRAINT "EventSignup_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "EventSignup" ADD CONSTRAINT "EventSignup_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "EventPosition"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EventSignup" ADD CONSTRAINT "EventSignup_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "EventPosition"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "EventPosition" ADD CONSTRAINT "EventPosition_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EventSignup" ADD CONSTRAINT "EventSignup_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Guest" ADD CONSTRAINT "Guest_signupId_fkey" FOREIGN KEY ("signupId") REFERENCES "EventSignup"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EventPosition" ADD CONSTRAINT "EventPosition_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "EventWaitlist" ADD CONSTRAINT "EventWaitlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Guest" ADD CONSTRAINT "Guest_signupId_fkey" FOREIGN KEY ("signupId") REFERENCES "EventSignup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "EventWaitlist" ADD CONSTRAINT "EventWaitlist_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "EventPosition"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EventWaitlist" ADD CONSTRAINT "EventWaitlist_positionId_fkey" FOREIGN KEY ("positionId") REFERENCES "EventPosition"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WaitlistGuest" ADD CONSTRAINT "WaitlistGuest_waitlistId_fkey" FOREIGN KEY ("waitlistId") REFERENCES "EventWaitlist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EventWaitlist" ADD CONSTRAINT "EventWaitlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WaitlistGuest" ADD CONSTRAINT "WaitlistGuest_waitlistId_fkey" FOREIGN KEY ("waitlistId") REFERENCES "EventWaitlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
