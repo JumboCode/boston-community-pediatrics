6d59645edd33e29177f9ac7f9a5c9e0efe0cf420
ManuP6789 <manuel.pena654576@tufts.edu>
2026-03-11 20:55:46 -0400

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,10 @@
 // lib/prisma.ts
 
-
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-  adapter: {
-    url: process.env.DATABASE_URL!,
-  },
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
 });
