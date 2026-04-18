a8bcd1fb97f80c782849b85ba2ca21d9a43ef399
tomasmaranga <tomas.maranga@icloud.com>
2026-04-18 15:19:41 -0400

    === shenanigans

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -154,7 +154,7 @@ export default async function EventDetailsPage(props: {
                 .filter(Boolean)
                 .join(", ");
 
-              if (user?.role == UserRole.ADMIN) {
+              if (user?.role === UserRole.ADMIN) {
                 return (
                   <EventAdminTable
                     key={item.id}
