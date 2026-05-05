e2037d27eb0e38dc1ee288e779dc44acb19a33d1
Tomas Maranga <73145481+tomasmaranga@users.noreply.github.com>
2026-05-05 17:00:43 -0400

    fixed back to manage roles button + made button reigstered (#115)

--- src/components/common/buttons/RegisterButton.tsx
+++ src/components/common/buttons/RegisterButton.tsx
@@ -7,22 +7,26 @@ interface RegisterButtonProps {
   positionId: string;
   disabled: boolean;
   isFull: boolean;
+  isRegistered: boolean;
 }
 
 export default function RegisterButton({
   positionId,
   disabled,
   isFull,
+  isRegistered,
 }: RegisterButtonProps) {
   const router = useRouter();
 
   return (
     <Button
-      label={isFull ? "Join Waitlist" : "Register"}
+      label={isRegistered ? "Registered" : isFull ? "Join Waitlist" : "Register"}
       disabled={disabled}
       onClick={() => router.push(`/register/${positionId}`)}
       altStyle={`mt-[12px] rounded font-medium flex items-center justify-center h-[40px] ${
-        isFull
+        isRegistered
+          ? "w-[120px] bg-gray-300 text-black hover:bg-gray-400 cursor-not-allowed"
+          : isFull
           ? "w-[120px] bg-gray-300 text-black hover:bg-gray-400"
           : "w-[94px] bg-light-bcp-blue text-white hover:bg-light-bcp-blue"
       }`}
